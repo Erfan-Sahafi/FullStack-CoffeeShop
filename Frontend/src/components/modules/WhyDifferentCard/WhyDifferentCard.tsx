9dce3c1e36a102c5937bf3164a88bd97c53b30fb
Erfan-Sahafi <erfansahafi9@gmail.com>
2024-06-06 17:59:31 +0330

    why diffrent section completed development

--- Frontend/src/components/modules/WhyDifferentCard/WhyDifferentCard.tsx
+++ Frontend/src/components/modules/WhyDifferentCard/WhyDifferentCard.tsx
@@ -1,12 +1,28 @@
+import Image from "next/image";
 import React from "react";
 
-const WhyDifferentCard = () => {
+interface whyCard {
+  iconsrc: String;
+  title: String;
+  desc: String;
+}
+
+const WhyDifferentCard = ({iconsrc,title,desc}:whyCard) => {
   return (
-    <div className="relative h-[364px] w-[280px] flex flex-col gap-3 font-playfairDisplay bg-[#FFF9F1] border-solid border-2 border-[#F9C06A]">
-      <div className="flex flex-col gap-2 items-center">
-        <p className="text-coffee text-[22px]">Expresso</p>
-        <p className="text-[16px] text-secondry">Coffee 50% | Milk 50%</p>
-        <p className="text-coffee font-bold text-[18px]">$8.50</p>
+    <div className="h-[284px] w-[280px] flex flex-col justify-center gap-3 font-playfairDisplay bg-[#FFEED8] border-solid border-2 border-[#F9C06A]">
+      <div className="flex flex-col gap-4 items-center">
+        <div>
+          <Image
+            width={88}
+            height={88}
+            alt="icon"
+            src={`${iconsrc}`}
+          />
+        </div>
+        <p className="text-[28px] font-bold text-coffee">{title}</p>
+        <p className="text-maingray w-40 text-center text-[18px]">
+         {desc}
+        </p>
       </div>
     </div>
   );
--- Frontend/src/components/templates/WhyDifferent/WhyDifferent.tsx
+++ Frontend/src/components/templates/WhyDifferent/WhyDifferent.tsx
@@ -1,5 +1,6 @@
 import CoffeeCard from "@/components/modules/CoffeeCard/CoffeeCard";
 import Title from "@/components/modules/Title/Title";
+import WhyDifferentCard from "@/components/modules/WhyDifferentCard/WhyDifferentCard";
 import React from "react";
 
 const WhyDifferent = () => {
@@ -10,10 +11,26 @@ const WhyDifferent = () => {
         We don’t just make your coffee, we make your day!
       </p>
       <div className="w-full flex justify-center gap-4">
-        <CoffeeCard />
-        <CoffeeCard />
-        <CoffeeCard />
-        <CoffeeCard />
+        <WhyDifferentCard
+          iconsrc={"/images/coffee-beans1.svg"}
+          title={"Supreme Beans"}
+          desc={"Beans that provides great taste"}
+        />
+        <WhyDifferentCard
+          iconsrc={"/images/badge1.svg"}
+          title={"High Quality"}
+          desc={"We provide the highest quality"}
+        />
+        <WhyDifferentCard
+          iconsrc={"/images/coffee-cup1.svg"}
+          title={"Extraordinary"}
+          desc={"Coffee like you have never tasted"}
+        />
+        <WhyDifferentCard
+          iconsrc={"/images/best-price1.svg"}
+          title={"Affordable Price"}
+          desc={"Our Coffee prices are easy to afford"}
+        />
       </div>
     </div>
   );
